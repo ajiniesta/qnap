948437106d58e1b7ff77504b3c617fe58ce372d7
Antonio Jose Iniesta <antonioj.iniesta@gmail.com>
2017-07-23 23:58:02 +0200

    Adding mkdir and copy functions

--- filestation.py
+++ filestation.py
@@ -114,3 +114,45 @@ class FileStation(Qnap):
                 )
             }
         )
+
+    def mkdir(self, target):
+        """
+        Create a directory
+        """
+        path_separator = "/"
+        destFolder = target.split(path_separator)[-1]
+        destPath = reduce(lambda x,y: x + path_separator + y, target.split(path_separator)[0:-1])
+        self.req(self.endpoint(
+            func='createdir',
+            params={
+                'dest_folder': destFolder,
+                'dest_path': destPath
+            }
+        ))
+
+    def mkdir_rec(self, target):
+        """
+        Creates a directory, and the intermediate ones
+        """
+        path_separator = "/"
+        reduce(lambda x,y: self.m2(x + path_separator + y), target.split(path_separator))
+
+    def m2(self, x):
+        self.mkdir(x)
+        return x
+
+    def copy(self, source_file, source_total=1, dest_file, mode=1, dup="copy"):
+        path_separator = "/"
+        file_name = target.split(path_separator)[-1]
+        file_folder_name = reduce(lambda x,y: x + path_separator + y, target.split(path_separator)[0:-1])
+        self.req(self.endpoint(
+            func='copy',
+            params={
+                'source_file':file_name,
+                'source_total':source_total,
+                'source_path':file_folder_name,
+                'dest_path':dest_file,
+                'mode':mode,
+                'dup':dup
+            }
+        ))
